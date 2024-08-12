598054ad6068158e9b8b000ed822882478239322
tarannagra <tarannagra31@gmail.com>
2024-08-12 01:59:47 +0100

    Added updated wezterm.lua

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -20,7 +20,9 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.95
 config.text_background_opacity = 0.8
 config.adjust_window_size_when_changing_font_size = false
-
+-- removes the title bar and buttons,
+-- makes for a really clean and minimal wezterm experience :D
+config.window_decorations = "RESIZE"
 
 -- terminal splitting
 config.leader = {
@@ -47,6 +49,36 @@ config.keys = {
         mods = "CTRL",
         action = wezterm.action.DecreaseFontSize
     },
+    {
+        key = "LeftArrow",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action{ActivatePaneDirection="Left"}
+    },
+    {
+        key = "RightArrow",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action{ActivatePaneDirection="Right"}
+    },
+    {
+        key = "DownArrow",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action{ActivatePaneDirection="Down"}
+    },
+    {
+        key = "UpArrow",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action{ActivatePaneDirection="Up"}
+    },
+    {
+        key = "F",
+        mods = "ALT",
+        action = act.ToggleFullScreen
+    },
+    {
+        key = "Enter",
+        mods = "ALT",
+        action = act.DisableDefaultAssignment
+    }
 }
 
 return config
