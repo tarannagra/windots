07b059964d9d4b0c6b8329f1ce115ce7e67c81d9
tarannagra <tarannagra31@gmail.com>
2024-09-23 23:22:16 +0100

    Updated: .wezterm.lua to use my new configuration :D

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -1,52 +1,92 @@
+--[[
+    Taran's Wezterm Configuration File.
+    Re-written on 23/09/24 for more clarity and added fun.
+--]]
+
 local wezterm = require 'wezterm'
-local muz = wezterm.mux
 local act = wezterm.action
 
 local config = {}
 
--- if a new version, then use the new method
--- else then default to the above config = {}
+-- Try to default to the config builder if possible
 if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
--- assigning the different configurations
+-- Changing the main options
+-- I like ChallengerDeep personally, but can be changed here too.
+-- Change default_prog to your executable:
+-- - pwsh || cmd || fish || bash || zsh
+
 config.default_prog = { 'pwsh' }
 config.color_scheme = "ChallengerDeep"
-config.use_fancy_tab_bar = true
--- ctrl+shift+t for a new tab
+config.font_size = 14.0
+
+-- Tabbing & custom tab bar
+-- I personally prefer no tab bar as I have a top bar with the info I would need
+config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
--- change the window opacity here, 1 is nice, but .9 is also nice
+
+-- Window properties
 config.window_background_opacity = 0.95
 config.text_background_opacity = 0.8
 config.adjust_window_size_when_changing_font_size = false
+config.window_decorations = "RESIZE"
+config.scrollback_lines = 5000
+config.enable_scroll_bar = false
 
+config.max_fps = 165
 
--- terminal splitting
-config.leader = {
-    key = 'a',
-    mods = "CTRL",
-    timeout_milliseconds = 1000
+-- Setting panes to dim if inactive 
+config.inactive_pane_hsb = {
+    saturation = 0.6,
+    brightness = 0.4
 }
+
+-- Configuring the leader key to C-a
+-- Leader is pressing C-a then another key for something to happen (within the timeout in ms)
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 5000}
 
 config.keys = {
-    -- This will create a new split and run your default program inside it
-    {
-        -- CTRL+SHIFT+ALT+" will split the window VERTICALLY
-        key = '"',
-        mods = 'CTRL|SHIFT|ALT',
-        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-    },
-    {
-        key = "=",
-        mods = "CTRL",
-        action = wezterm.action.IncreaseFontSize
-    },
-    {
-        key = "-",
-        mods = "CTRL",
-        action = wezterm.action.DecreaseFontSize
-    },
+    -- LEADER+c :: Copy mode enabled 
+    { key = "c", mods = "LEADER", action = act.ActivateCopyMode },
+
+    -- Pane bindings
+    { key = "/", mods = "LEADER", action = act.SplitHorizontal { domain = "CurrentPaneDomain"} },
+    { key = "\\", mods = "LEADER", action = act.SplitVertical { domain = "CurrentPaneDomain"} },
+    { key = "x", mods = "LEADER", action = act.CloseCurrentPane { confirm = true } },
+    -- For navigating the panes, this is what I am used to and I'm pretty sure it's the default anyways
+    { key = "LeftArrow", mods = "CTRL|SHIFT", action = wezterm.action{ActivatePaneDirection="Left"} },
+    { key = "RightArrow", mods = "CTRL|SHIFT", action = wezterm.action{ActivatePaneDirection="Right"} },
+    { key = "DownArrow", mods = "CTRL|SHIFT", action = wezterm.action{ActivatePaneDirection="Down"} },
+    { key = "UpArrow", mods = "CTRL|SHIFT", action = wezterm.action{ActivatePaneDirection="Up"} },
+
+    -- Maximise the current pane, keeping the others in the background,
+    -- same key to minimise and return back to normal, hence the 'toggle'
+    { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+    -- Rotate the panes (Counter/Clock)wise
+    { key = "n", mods = "LEADER", action = act.RotatePanes 'Clockwise' },
+    { key = "m", mods = "LEADER", action = act.RotatePanes 'CounterClockwise' }
 }
+
+-- Uncomment this for a pretty cool Wezterm bar at the bottom
+-- -- using a terminal bar at the bottom for info
+-- local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+-- bar.apply_to_config(
+--     config,
+--     {
+--         position = "top",
+--         max_width = 20,
+        
+--         enabled_modules = {
+--             username = true,
+--             clock = true,
+--             workspace = false,
+--             pane = false,
+--             hostname = false,
+--             cwd = false -- cwd->current working directory
+--         }
+--     }
+-- )
 
 return config
