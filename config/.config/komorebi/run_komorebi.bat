9e46ca396862c2b1bcd77f2c089a29f5be125008
tarannagra <tarannagra@proton.me>
2025-03-07 21:50:01 +0000

    Modified: run_komorebi.bat for each bar

--- config/.config/komorebi/run_komorebi.bat
+++ config/.config/komorebi/run_komorebi.bat
@@ -1,3 +1,5 @@
 @echo off
 
-komorebic start --whkd
+komorebic start --masir
+pwsh -C Start-Process "komorebi-bar" '"--config" "C:\Users\subwa\.config\komorebi\komorebi.bar.json"' -WindowStyle hidden
+pwsh -C Start-Process "komorebi-bar" '"--config" "C:\Users\subwa\.config\komorebi\komorebi.bar.2.json"' -WindowStyle hidden
